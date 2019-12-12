abbabdcd23bad2fcef20cb954e499a2cb55f2b2d
matthrx <matthieu_upmc@outlook.fr>
2019-12-12 23:28:53 +0100

    Theoretical aspect of code (soft) is likely to be done .TESTODO ! //

--- SOFT_DECODEUR_GROUPE_4.m
+++ SOFT_DECODEUR_GROUPE_4.m
@@ -1,38 +1,51 @@
+function parity = parity_checked(c, H)
+      % fonction return true si parité n'est pas vérfifée, false sinon
+      parity = true;
+      test_vector = H*c;
+      if mod(test_vector, 2) ~= zeros(size(test_vector,1), size(test_vector,2))
+          parity = false;
+      
+      end
+end
+
 function vector = HARD_DECODEUR_GROUPE_41(c, H, p, MAX_ITER)
-    % p array ligne
-    %Initialisation de la matrice q regroupant les rÃ©ponses de c_i Ã  f_j
-    %(voir doc)
-    % j sera en rÃ©alitÃ© le nombre de "check node" et i sera le nombre de
-    % v-node
-    matrice_q = 
-    
-    % On stocke uniquement les q(1) // r et q ont la mÃªme taille cÃ d c
-    matrice_r = zeros(size(c,2), size(c,1));  
-    rows_q = zeros(size(c,1)); 
-    for x=1:size(matrice_r,1)
-       rows_q(x) = x;
-    end
-    disp(rows_q)
-    ite=0;
-    while(ite<MAX_ITER)
+   
+    M = size(c,1);
+    N = size(H,1);
+    i = 0;
+    while(i<MAX_ITER && ~parity_not_checked(c, H))
         
-        % Calcul du message de rÃ©ponse de la part des noeuds. 
-        for col=1:size(matrice_r,2)
-            for row=1:size(matrice_r,1)
-                V_ji = rows_q;
-                V_ji(row) = [];
-                produit = 1;
-                for i_tild=V_ji
-                  produit = produit*(1-2*matrice_q(i_tild,col));
-                end
-                matrice_r(col,row)= 0.5*(1-produit);
-               % On stocke que les 1 dans matrice_r
+        for j = 1:M %On itère dans le mot code reçu
+            coeff_r = []; % A chaque 
+            coeff_r(1) = p(j); %Aucune autre information dispo, initialisation
+            
+            for k = 1:N
+               % On checke que le noeud est bien un noeud de vérification afin 
+               % d'en déduire que l'on va recevoir un message (page 4)
+               if H(k,j) == 1
+                  produit = 1;
+                  array_without_k = setdiff(1:N,k); % On a donc Vj\i et donc i'
+                  for l = array_without_k
+                        if H(k,l) == 1 % On réinspecte qu'un message sera reçu en ce noeud
+                            produit = produit*(1-2*p(l)); % On applique une partie de la formule 3
+                        end
+                  end
+                  coeff_r = [coeff_r 0.5*(1-produit)]; %On stocke tous les coeffs calculés à l'itération j (mot code)
+               end
+            end 
+            q_1 = p(j)*prod(coeff_r);
+            p_bis(i) = q_1/(q_1+q_0); %Formule 5 et 6
+            q_0 = (1-p(j))*prod(1-coeff_r);
+            if (q_1/(q_0+q_1) > 0.5)
+                c(i) = 0;
+            else
+                c(i)=1;
             end
         end
-        
-        for row=1:size(matrice_q,1)
-            for col=1:size(matrice_q,2)
-                C_ij = rows
+        %Fin d'itération, autre condition pour quit ? - On actualise 
+        p = p_bis;
+        % si le nouveau mot code remplit la condition de parité over sinon
+        % on réitère (while)
     end
     vector=c;
 end
