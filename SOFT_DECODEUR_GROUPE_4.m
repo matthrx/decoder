5cef91cc784f9195ca682f7cc6b2b8b3803242d7
matthrx <matthieu_upmc@outlook.fr>
2019-12-19 00:30:50 +0100

    Version commited by Quentin (supposed to be debug + start of solution for soft decoder

--- SOFT_DECODEUR_GROUPE_4.m
+++ SOFT_DECODEUR_GROUPE_4.m
@@ -1,21 +1,15 @@
-function parity = parity_checked(c, H)
-      % fonction return true si parité n'est pas vérfifée, false sinon
-      parity = true;
-      test_vector = H*c;
-      if mod(test_vector, 2) ~= zeros(size(test_vector,1), size(test_vector,2))
-          parity = false;
-      
-      end
-end
 
-function vector = HARD_DECODEUR_GROUPE_41(c, H, p, MAX_ITER)
-   
+
+function vector = HARD_DECODEUR_GROUPE_4(c, H, p, MAX_ITER)
+   %test
     M = size(c,1);
     N = size(H,1);
-    i = 0;
-    while(i<MAX_ITER && ~parity_not_checked(c, H))
+    i = 1;
+    %while(i<MAX_ITER && parity_checked(c, H))
+        p_bis=[];
         
         for j = 1:M %On itère dans le mot code reçu
+            
             coeff_r = []; % A chaque 
             coeff_r(1) = p(j); %Aucune autre information dispo, initialisation
             
@@ -25,28 +19,48 @@ function vector = HARD_DECODEUR_GROUPE_41(c, H, p, MAX_ITER)
                if H(k,j) == 1
                   produit = 1;
                   array_without_k = setdiff(1:N,k); % On a donc Vj\i et donc i'
-                  for l = array_without_k
-                        if H(k,l) == 1 % On réinspecte qu'un message sera reçu en ce noeud
-                            produit = produit*(1-2*p(l)); % On applique une partie de la formule 3
+
+                  for x = array_without_k
+                        if H(k,x) == 1 % On réinspecte qu'un message sera reçu en ce noeud
+                            produit = produit*(1-2*p(x)); % On applique une partie de la formule 3
                         end
                   end
                   coeff_r = [coeff_r 0.5*(1-produit)]; %On stocke tous les coeffs calculés à l'itération j (mot code)
                end
-            end 
-            q_1 = p(j)*prod(coeff_r);
-            p_bis(i) = q_1/(q_1+q_0); %Formule 5 et 6
-            q_0 = (1-p(j))*prod(1-coeff_r);
-            if (q_1/(q_0+q_1) > 0.5)
-                c(i) = 0;
-            else
-                c(i)=1;
             end
+            
+            q_1 = p(j)*prod(coeff_r);
+            q_0 = (1-p(j))*prod(1-coeff_r);
+            
+            p_bis(j) = q_1/(q_1+q_0); %Formule 5 et 6
+            
+            
+            if (q_1/(q_0+q_1) > 0.5)
+                c(j) = 0;
+            else
+                c(j)=1;
+            end
+            disp value_of_p
+            disp(j)
+            disp(p_bis(j))
+            
         end
+        disp pbis
+        disp(p_bis)
         %Fin d'itération, autre condition pour quit ? - On actualise 
         p = p_bis;
         % si le nouveau mot code remplit la condition de parité over sinon
         % on réitère (while)
-    end
+    %end
     vector=c;
 end
 
+function parity = parity_checked(c, H)
+      % fonction return true si parité n'est pas vérfifée, false sinon
+      parity = true;
+      test_vector = H*c;
+      if mod(test_vector, 2) ~= zeros(size(test_vector,1), size(test_vector,2))
+          parity = false;
+      
+      end
+end
