7f15aa6fff9fac859f31c12b6125e34e77f8c176
matthrx <matthieu_upmc@outlook.fr>
2019-12-22 01:31:55 +0100

    Last version review by Quentin & me. Ready to be sent, waiting for the CR // readme

--- SOFT_DECODEUR_GROUPE_4.m
+++ SOFT_DECODEUR_GROUPE_4.m
@@ -1,52 +1,127 @@
+function vector = SOFT_DECODEUR_GROUPE_41(c, H, p, MAX_ITER)
+    % c est un vecteur colonne, p est une 
+    % constante aussi vecteur colonne
+    
+    %récupération des informations sur nos données
+    lignes_H = size(H,1);
+    colonne_c = size(c,1);
+    %les messages de nos values nodes vers nos check nodes
+    Qij = [];
+    iter = 0;
+    %initialisation de nos messages envoyés pour correspondre aux
+    %probabilités initiales
+    for i = 1:lignes_H
+        Qij = [Qij;p'];
+    end
+    
+    
+    
+    %itération tant que notre mot n'est pas décodé ou qu'on a atteint le
+    %nombre max d'itérations
+    while (iter<MAX_ITER && ~parity_checked(c,H))
+        %vérification que l'on envoie des messages uniquement aux bonnes
+        %check nodes
+        Qij = Qij.*H;
+        disp(Qij)
+        %initialisation de la réponse de nos check nodes
+        Rij_0=[];
+        
+        %parcours de notre matrice Qij ligne par ligne, ou check node par check node
+        for k = 1:lignes_H
+            %k=j
+            %initialisation des réponses de notre check node
+            vecteur_Rij_0 = zeros(1,colonne_c);
+            %parcours de notre node élément par élément
+            for l = 1:colonne_c
+                %l=i
+                %vérification que l'on doit traiter notre value node
+                if H(k,l) ~= 0
+                    produit1 = 1;
+                    %récupération des indexes sauf celui de la node traitée
+                    %nommé Vj\i dans le papier
+                    array_without_k = setdiff(1:colonne_c, l);
+                    for m = array_without_k
+                        %calcul de notre réponse, rij(0), en fonction des
+                        %qij(1)
+                        produit1 = produit1*(1-2*Qij(k,m));%formule 3, partie produit
+                    end
+                    %stockage de nos réponses dans un vecteur
+                    vecteur_Rij_0(l) = 0.5+0.5*produit1;%fin de la formule 3
+                end
+            end
+            Rij_0=[Rij_0;vecteur_Rij_0];
+        end
+        Rij_1=1-Rij_0;
+        % Fin calcul de Rij
+        %Rij confirmé bon sur un exemple à la main
+        
+        disp Rij_1
+        disp(Rij_1)
+        % Calcul de Q
+        %parcours de Rij value node par value node
+        for k = 1:colonne_c
+            %valeurs pour les formules 7 & 8
+            Qi_0=1;
+            Qi_1=1;
+            %itération de Rij check node par check node
+            for l = 1:lignes_H
+                produit1 = 1;%qij(1)
+                produit0=1;%qij(0)
+                
+                if H(l,k)~=0%check si les check nodes devaient envoyer une réponse
+                    array_without_k = setdiff(1:lignes_H, l); %All nodes except f_j
+                    for m = array_without_k
+                        if H(m, k)~=0
+                            %calcul des produits des formules 5 & 6
+                            if H(m,k)~=0
+                                produit0=produit0*Rij_0(m,k);%qij(0)
+                                produit1=produit1*Rij_1(m,k);%qij(1)
+                            end
+                        end
+                    end
+                    %application des probabilités initiales à notre produit
+                    produit0=produit0*(1-p(k));
+                    produit1=produit1*p(k);
+                    Kij=1/(produit0+produit1);%facteur pour que somme des probas=1
+                    produit0=produit0*Kij;
+                    produit1=produit1*Kij;
+                    Qij(l, k)=produit1;%fin de la formule 5
+                    
+                    %Qij validé par un calcul à la main
+                    
+                    %mise a jour de notre produit final, sans se préoccuper
+                    %des array_without_k
+                    Qi_0=Qi_0*Rij_0(l,k);
+                    Qi_1=Qi_1*Rij_1(l,k);
+                end
+            end%fin d'itération sur les colonnes(aka check node)
+            
+            %fin des formules 7 & 8
+            Ki=1/(Qi_0+Qi_1);
+            Qi_0=Ki*(1-p(k))*Qi_0;
+            Qi_1=Ki*p(k)*Qi_1;
+            if Qi_1>Qi_0
+                c(k)=1;
+            else
+               c(k)=0;
+            end     
+        end
+        disp Qij
+        disp(Qij)
+        disp(p')
+        iter=iter+1;
+    end
+    %disp(Qij)
+    vector=c;
+end
+
 function parity = parity_checked(c, H)
       % fonction return true si parité n'est pas vérfifée, false sinon
       parity = true;
       test_vector = H*c;
-      if mod(test_vector, 2) ~= zeros(size(test_vector,1), size(test_vector,2))
-          parity = false;
-      
+      for x = 1:size(test_vector,1)
+          if mod(test_vector(x,1),2)==1
+               parity = false;
+          end
       end
 end
-
-function vector = HARD_DECODEUR_GROUPE_41(c, H, p, MAX_ITER)
-   
-    M = size(c,1);
-    N = size(H,1);
-    i = 0;
-    while(i<MAX_ITER && ~parity_not_checked(c, H))
-        
-        for j = 1:M %On itère dans le mot code reçu
-            coeff_r = []; % A chaque 
-            coeff_r(1) = p(j); %Aucune autre information dispo, initialisation
-            
-            for k = 1:N
-               % On checke que le noeud est bien un noeud de vérification afin 
-               % d'en déduire que l'on va recevoir un message (page 4)
-               if H(k,j) == 1
-                  produit = 1;
-                  array_without_k = setdiff(1:N,k); % On a donc Vj\i et donc i'
-                  for l = array_without_k
-                        if H(k,l) == 1 % On réinspecte qu'un message sera reçu en ce noeud
-                            produit = produit*(1-2*p(l)); % On applique une partie de la formule 3
-                        end
-                  end
-                  coeff_r = [coeff_r 0.5*(1-produit)]; %On stocke tous les coeffs calculés à l'itération j (mot code)
-               end
-            end 
-            q_1 = p(j)*prod(coeff_r);
-            p_bis(i) = q_1/(q_1+q_0); %Formule 5 et 6
-            q_0 = (1-p(j))*prod(1-coeff_r);
-            if (q_1/(q_0+q_1) > 0.5)
-                c(i) = 0;
-            else
-                c(i)=1;
-            end
-        end
-        %Fin d'itération, autre condition pour quit ? - On actualise 
-        p = p_bis;
-        % si le nouveau mot code remplit la condition de parité over sinon
-        % on réitère (while)
-    end
-    vector=c;
-end
-
